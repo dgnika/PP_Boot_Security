54a392538e0b315dcc226cb1f048e1d51276b732
veronika <112850674+dgnika@users.noreply.github.com>
2026-06-05 05:41:14 +0300

    Create InitUsers.java

---
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/InitUsers.java
@@ -0,0 +1,47 @@
+@Component
+public class InitUsers {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    private final PasswordEncoder passwordEncoder;
+    public InitUsers(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    @Transactional
+    public void initTestUsers() {
+        Role roleAdmin = entityManager.createQuery(
+                        "select r from Role r where r.role = :role", Role.class)
+                .setParameter("role", "ROLE_ADMIN")
+                .getResultStream()
+                .findFirst()
+                .orElseGet(() -> {
+                    Role r = new Role("ROLE_ADMIN");
+                    entityManager.persist(r);
+                    return r;
+                });
+
+        Role roleUser = entityManager.createQuery(
+                        "select r from Role r where r.role = :role", Role.class)
+                .setParameter("role", "ROLE_USER")
+                .getResultStream()
+                .findFirst()
+                .orElseGet(() -> {
+                    Role r = new Role("ROLE_USER");
+                    entityManager.persist(r);
+                    return r;
+                });
+
+        User user1 = new User("admin", "Veronika", "Darbinyan", 22L, "admin");
+        user1.setPassword(passwordEncoder.encode("admin"));
+        user1.setRoles(new HashSet<>(Set.of(roleAdmin, roleUser)));
+
+        User user2 = new User("user", "Ivan", "Ivanov", 20L, "user");
+        user2.setPassword(passwordEncoder.encode("user"));
+        user2.setRoles(new HashSet<>(Set.of(roleUser)));
+
+        entityManager.persist(user1);
+        entityManager.persist(user2);
+    }
+}
